b9e9445cbf9464c962df4488b5d5fd5379eda5bc
Isaac Obuya <Szack4477@gmail.com>
2020-04-21 11:17:31 +0300

    display NewTransaction widget in a Modal bottom sheet and lift state to main widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+
+import './models/transaction.dart';
+import './widgets/new_transactions.dart';
+import './widgets/transaction_list.dart';
 
 
 void main() => runApp(MyApp());
@@ -18,7 +21,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  // Method to start the process for displaying the modal for adding a new transaction
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _userTransaction = [
+ Transaction(id: 't1',title: 'New trouser',amount: 500.00, date: DateTime.now()),
+   Transaction(id: 't2',title: 'New shirt',amount: 250.45, date: DateTime.now()),
+   Transaction(id: 't3',title: 'New shoes',amount: 850.00, date: DateTime.now())
+  ];
+
+  // method to add a new transaction
+  void _addNewTransaction(String title, double amount) {
+    final newTransaction = new Transaction(id: DateTime.now().toString(), title: title, amount: amount, date: DateTime.now());
+
+    // we now need to update the transaction list
+    setState(() {
+      _userTransaction.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    // This method requires context which we get from the root widget
+    // The builder returns the new widget to be displayed in the modal
+    // it optional requires context thus we can pass underscore to mean that we do not want to use it
+    showModalBottomSheet(context: ctx, builder: (_) {
+        return GestureDetector(
+          onTap: (){},
+          behavior: HitTestBehavior.opaque,
+          child: NewTransaction(_addNewTransaction),);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +65,8 @@ class MyHomePage extends StatelessWidget {
         title: Text("Expense Tracker App"),
         actions: <Widget>[
            IconButton(icon: Icon(Icons.add, color: Colors.white,), 
-           onPressed: (){})
+           onPressed: () => _startAddNewTransaction(context)
+           )
         ],
       ),
       body: SingleChildScrollView(
@@ -44,13 +83,13 @@ class MyHomePage extends StatelessWidget {
              elevation: 5,
            ),
            ),
-         UserTransactions()
+         TransactionList(transactions: _userTransaction,),
          ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () =>_startAddNewTransaction(context),
         child: Icon(Icons.add),
       )
     );
