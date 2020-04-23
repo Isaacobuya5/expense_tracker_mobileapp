f851b34f49c5a2f7660b5808f01f61b047a41aca
Isaac Obuya <Szack4477@gmail.com>
2020-04-23 09:18:00 +0300

    create chart bar component displayed per transaction

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
 
@@ -24,19 +25,29 @@ class Chart extends StatelessWidget {
        }
       }
 
-      // test
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
+      return { 
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum};
+    });
+  }
 
-      return { 'day': DateFormat.E().format(weekDay),'amount': totalSum};
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+       return sum + item['amount'];
     });
   }
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
+      child: Row(children: groupedTransactionValues.map((data) {
+       return Chartbar(
+         data['day'], 
+         data['amount'], 
+         totalSpending == 0 ? 0.0 : (data['amount'] as double)/ totalSpending);
+      }).toList()
+      ),
     );
   }
 }
